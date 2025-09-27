f02d2618bba900814d404aa8af07ee81f3e3804e
Danila Vasilev <mr.den.vi@gmail.com>
2025-09-27 16:52:40 +0200

    --allow-empty

--- config/nvim/lua/config/keymap.lua
+++ config/nvim/lua/config/keymap.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>rf', builtin.buffers, { desc = 'Telescope recent files' })
+vim.keymap.set('n', '<leader>cd', function() 
+    vim.cmd('cd %:p:h')
+end, { desc = 'cd to current file\'s directory' })
 
 vim.keymap.set('n', '<C-_>', function()
     return vim.v.count == 0
